d7774229ed52651771d9578b688ca2b6b29648a9
Ingo Albers <pjotrpansen@gmail.com>
2017-02-11 15:15:55 +0100

    Handle brain activity in separate method

--- www/js/app.js
+++ www/js/app.js
@@ -4,21 +4,8 @@ angular.module('starter', ['ionic'])
   $scope.recognizedText = '';
 
   $scope.speakText = function() {
-    switch($scope.recognizedText) {
-      case 'hello':
-        text = 'Ja Hallo!';
-        break;
-      default:
-        text = 'Hä!';
-    }
-
-    // Gain sudden clarity
-    if(Math.floor(Math.random() * 10 + 1) == 10) {
-      text = 'Ah ja ok!';
-    }
-
     TTS.speak({
-      text: text,
+      text: $scope.processInput($scope.recognizedText),
       locale: 'de-DE',
       rate: 1.5
     }, function() {
@@ -26,7 +13,21 @@ angular.module('starter', ['ionic'])
     }, function(reason) {
       // Handle the error case
     });
-  }
+  };
+
+  $scope.processInput = function(text) {
+    // 10% chance to gain sudden clarity
+    if(Math.floor(Math.random() * 10 + 1) == 10) {
+      return 'Ah ja ok!';
+    }
+
+    switch(text) {
+      case 'hello':
+        return 'Ja Hallo!';
+      default:
+        return 'Hä!';
+    }
+  };
 
   $scope.record = function() {
     var recognition = new SpeechRecognition();
