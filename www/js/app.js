f6ea1b7d100dd97849659acedb1487dee09cfce7
Ingo Albers <pjotrpansen@gmail.com>
2017-02-11 15:09:24 +0100

    Let Manfred answer automatically

--- www/js/app.js
+++ www/js/app.js
@@ -1,30 +1,41 @@
 angular.module('starter', ['ionic'])
 
 .controller('AppCtrl', function($scope) {
-  $scope.data = {
-    speechText: ''
-  };
   $scope.recognizedText = '';
 
   $scope.speakText = function() {
+    switch($scope.recognizedText) {
+      case 'hello':
+        text = 'Ja Hallo!';
+        break;
+      default:
+        text = 'Hä!';
+    }
+
+    // Gain sudden clarity
+    if(Math.floor(Math.random() * 10 + 1) == 10) {
+      text = 'Ah ja ok!';
+    }
+
     TTS.speak({
-           text: $scope.data.speechText,
-           locale: 'en-GB',
-           rate: 1.5
-       }, function () {
-           // Do Something after success
-       }, function (reason) {
-           // Handle the error case
-       });
-  };
+      text: text,
+      locale: 'de-DE',
+      rate: 1.5
+    }, function() {
+      // Do Something after success
+    }, function(reason) {
+      // Handle the error case
+    });
+  }
 
   $scope.record = function() {
     var recognition = new SpeechRecognition();
     recognition.onresult = function(event) {
-        if (event.results.length > 0) {
-            $scope.recognizedText = event.results[0][0].transcript;
-            $scope.$apply()
-        }
+      if (event.results.length > 0) {
+        $scope.recognizedText = event.results[0][0].transcript;
+        $scope.$apply()
+        $scope.speakText();
+      }
     };
     recognition.start();
   };
